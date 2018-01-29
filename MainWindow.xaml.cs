72d9f213f12b3e8e5bd990ef05b3957aa7b885a6
andrei <n.andrei@gmail.com>
2018-01-29 00:14:43 -0800

    made all button event handlers (empty)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,112 @@ namespace CameraController
         {
             InitializeComponent();
         }
+
+        # region Button Even Handlers
+        private void StartCam_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Capture_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExposureMinus2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExposureMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExposurePlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExposurePlus2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DurationMinus2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DurationPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DurationPlus2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FramesMinus2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FramesMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FramesPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FramesPlus2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RateMinus2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RateMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RatePlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RatePlus2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TimerMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TimerPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        # endregion
     }
 }
